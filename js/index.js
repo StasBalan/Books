5592656b4c4a574c0cb0dd49e4cb70745cd8c761
StasBalan <star9kmmr@gmail.com>
2019-05-29 16:55:21 +0300

    started doing the routing

--- js/index.js
+++ js/index.js
@@ -10,3 +10,38 @@ headerTitle.innerText = 'LOGO BOOK';
 //compiling header
 header.appendChild(container);
 container.appendChild(headerTitle);
+
+//routing on js
+let view = document.getElementById('output');
+
+function Router (name, routes) {
+    return {
+        name: name,
+        routes: routes
+    }
+}
+
+let myFirstRouting = new Router('myFirstRouting', [
+        {
+            path: '/',
+            name: 'Main'
+        },
+        {
+            path: '/contact',
+            name: 'Contact'
+        },
+        {
+            path: '/about',
+            name: 'About'
+        }
+    ]
+);
+
+let currentPath = window.location.pathname;
+if(currentPath === '/') {
+    view.innerHTML = 'GOGOGO';
+}else {
+
+}
+
+console.log(myFirstRouting);
